7f881b479b92cc0cfabf2b2b0a3b888275e6c3fa
slndrG <136209610+slndrG@users.noreply.github.com>
2024-07-04 21:14:36 +0545

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the Kishani App.</Text>
+      <Text>Welcome to the Kishani .</Text>
       <StatusBar style="auto" />
     </View>
   );
