b6c1c313080be3a9bc5f7e9f68e5e8b065e6f350
Sagar Karkee <131221453+SagarKarkee@users.noreply.github.com>
2024-07-04 22:10:05 +0545

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'rgp 9,2,8',
     alignItems: 'center',
     justifyContent: 'center',
   },
