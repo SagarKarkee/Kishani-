eeda7771ff70dda9cbbcfc7a24b529f54b071c27
Sagar Karkee <131221453+SagarKarkee@users.noreply.github.com>
2024-07-04 22:09:11 +0545

    Update App.js
    finally vayo ll kta ho

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Kishani App.</Text>
+      <Text>hello mula ko sak</Text>
       <StatusBar style="auto" />
     </View>
   );
