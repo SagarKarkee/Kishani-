7fd0e28fc2fd1801c31559703583838f56c4233d
slndrG <136209610+slndrG@users.noreply.github.com>
2024-07-05 10:09:44 +0545

    Hello kt ho

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Kishani App.</Text>
-      <Text>hello mula ko sak</Text>
       <StatusBar style="auto" />
     </View>
   );
