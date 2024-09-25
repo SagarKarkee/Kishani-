05da393d987079454d2ceacf0b6eeda9c0f3540e
Sandip112345 <122189505+Sandip112345@users.noreply.github.com>
2024-09-25 20:20:37 +0545

    modified farmers' login schema

--- FarmersLogin.js
+++ FarmersLogin.js
@@ -1,23 +1,9 @@
 const mongoose = require('mongoose');
 
-// Define schema for Farmers login credentials
-const FarmersLoginSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+const farmerLoginSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
 });
 
-// Create the model
-const FarmersLogin = mongoose.model('FarmersLogin', FarmersLoginSchema, 'LoginCredentials');
-
-module.exports = FarmersLogin;
+module.exports = mongoose.model('FarmersLogin', farmerLoginSchema);
