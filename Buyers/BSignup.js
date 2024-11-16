6508c0fd5b969dfd5b16d21978bafe7beab0654a
sagarkarkee <karkee9868@gmail.com>
2024-11-16 20:55:37 +0545

    s

--- Buyers/BSignup.js
+++ Buyers/BSignup.js
@@ -5,8 +5,8 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import axios from 'axios';
 // import API_URL from '../config';
 
-
-const API_URL = 'http://192.168.1.81:5000';
+const API_URL="http://192.168.1.68:5000";
+// const API_URL = 'http://192.168.1.81:5000';
 
 const BSignup = ({ navigation }) => {
   const [fullName, setFullName] = useState('');
