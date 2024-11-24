60c391c82c16c6525c1b16dfed8c5a06c6f9d3f9
Sandip112345 <sandip2211175@iimscollege.edu.np>
2024-11-24 13:58:46 +0545

    api

--- src/CropRecommendation.js
+++ src/CropRecommendation.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
 
+const API_URL = process.env.API_URL;
+console.log("Api url for addproduct: ", API_URL);
+
 const CropRecommendation = () => {
   // State for form data
   const [nitrogen, setNitrogen] = useState('');
@@ -13,8 +16,7 @@ const CropRecommendation = () => {
   const [rainfall, setRainfall] = useState('');
   const [result, setResult] = useState('');
 
-  // API URL from your Flask server
-  const API_URL = "http://192.168.0.102:5000/"; // Change to your IP address or URL
+  
 
   // Handle form submission
   const handleSubmit = async () => {
